29f534aad9af0855ff690930d7fd666fcaf5244c
James Batt <place.net.au@gmail.com>
2016-04-19 10:05:20 +1000

    added eslint to the project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+	static propTypes = {
+
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>hello world</h1>
 			</div>
-		)
+		);
 	}
 }
 
